0e17a318ffe1a53116c4bba56de4f7cee31945bd
Ramzan <ramzan.tumgojev@openvalue.at>
2022-11-17 11:53:11 +0100

    testing app.component commit #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,75 @@
-import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { QuestionService } from "./service/question/question.service";
+import { TeacherService } from "./service/teachers/teacher.service";
+import { createMock, Mock } from '@testing-library/angular/jest-utils';
+import { SignInComponent } from "./sign-in/sign-in.component";
+import { MockComponent } from "ng-mocks";
+import { QuestionBrowserComponent } from "./question-browser/question-browser.component";
+import { render, screen } from '@testing-library/angular';
+import { of } from "rxjs";
+import { Question } from "./model/Question";
+import userEvent from "@testing-library/user-event";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { CommonModule } from "@angular/common";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  it('should show all questions if selected all questions option', async () => {
+    const { container, questionService } = await renderComponent();
+    questionService.loadQuestions.mockReturnValue(
+      of([{} as Question, {} as Question])
+    );
+    const allQuestionsLink = screen.getByText('Questions');
+
+    await userEvent.click(allQuestionsLink);
+
+    expect(container.querySelectorAll('app-question-browser').length).toBe(1);
+    expect(container.querySelectorAll('app-question-list-item').length).toBe(2);
+  });
+
+  function createMocks() {
+    const questionService: Mock<QuestionService> = createMock(QuestionService);
+    const teacherService: Mock<TeacherService> = createMock(TeacherService);
+
+    return { questionService, teacherService };
+  }
+
+  async function renderWithMock(
+    questionService: Mock<QuestionService>,
+    teacherService: Mock<TeacherService>
+  ) {
+    const { container, detectChanges } = await render(AppComponent, {
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([
+          { path: 'questions', component: QuestionBrowserComponent },
+        ]),
       ],
       declarations: [
-        AppComponent
+        NavbarComponent,
+        CommonModule,
+        MockComponent(SignInComponent),
       ],
-    }).compileComponents();
-  });
+      componentProviders: [
+        {
+          provide: QuestionService,
+          useValue: questionService,
+        },
+        {
+          provide: TeacherService,
+          useValue: teacherService,
+        },
+      ],
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    return { container, detectChanges };
+  }
 
-  it(`should have as title 'q-and-a'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('q-and-a');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('q-and-a app is running!');
-  });
+  async function renderComponent() {
+    const { questionService, teacherService } = createMocks();
+    const { container, detectChanges } = await renderWithMock(
+      questionService,
+      teacherService
+    );
+    return { container, detectChanges, questionService, teacherService };
+  }
 });
